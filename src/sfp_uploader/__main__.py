2a11d3a58dc9e7f2248bb85925f4ccf3775a5c29
OhkuboSGMS <ginger.os777146th@gmail.com>
2024-03-27 23:42:32 +0900

    upgrade to 0.1.2
    引数を追加
    * 公開するかどうかを選択可能
    * サムネイルを変更可能

--- src/sfp_uploader/__main__.py
+++ src/sfp_uploader/__main__.py
@@ -2,7 +2,7 @@ import argparse
 import asyncio
 from datetime import datetime
 
-from .main import publish
+from sfp_uploader.main import publish
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Process URL, email, password, and audio file path.")
@@ -15,8 +15,11 @@ if __name__ == '__main__':
     parser.add_argument('--description', '-d', type=str, required=True, help='Description of the data')
     parser.add_argument('--schedule', '-s', type=datetime.fromisoformat, help='Schedule of the data')
     parser.add_argument('--explicit', '-e', action="store_true", help='Explicit of the data')
+    parser.add_argument('--promotional', '-p', action="store_true", help='Promotional of the data')
+    parser.add_argument('--thumbnail', '-th', type=str, help='Path to the image file')
+    parser.add_argument('--not_publish', '-np', action="store_false", help='Publish of the data')
     args = parser.parse_args()
     result = asyncio.run(
         publish(args.url, args.email, args.password, args.audio_file_path, args.title, args.description,
-                args.schedule, args.explicit))
+                args.schedule, args.explicit, args.promotional, args.thumbnail, args.not_publish))
     print(f"Share URL: {result}")
--- src/sfp_uploader/main.py
+++ src/sfp_uploader/main.py
@@ -1,3 +1,4 @@
+import os
 from datetime import datetime
 from typing import Optional
 
@@ -7,7 +8,8 @@ _publish_url = "https://podcasters.spotify.com/pod/dashboard/episodes"
 
 
 async def publish(url: str, email: str, password: str, audio_file: str, title: str, description: str,
-                  schedule: Optional[datetime] = None, explicit: bool = False, promotional: bool = False):
+                  schedule: Optional[datetime] = None, explicit: bool = False, promotional: bool = False,
+                  thumbnail: Optional[str] = None, is_publish: bool = True):
     async with async_playwright() as p:
         browser = await p.chromium.launch(headless=False)
         context = await browser.new_context()
@@ -20,18 +22,19 @@ async def publish(url: str, email: str, password: str, audio_file: str, title: s
         await context.grant_permissions(['clipboard-read', 'clipboard-write', 'accessibility-events'])
 
         page = await context.new_page()
+        # ログイン
         await page.goto(url)
-
         await page.get_by_role("button", name="Continue", exact=True).click()
         await page.get_by_role("textbox", name="Enter your email").fill(email)
         await page.get_by_role("textbox", name="Enter your password").fill(password)
         await page.get_by_role("button", name="Log in", exact=True).click()
+        # エピソードの追加
         await page.get_by_role("link", name="New Episode").click()
         async with page.expect_file_chooser() as fc_info:
-            await page.get_by_role("button", name="Select a file").click()
+            await page.get_by_role("button", name="Select a file", exact=True).click()
         file_chooser = await fc_info.value
         await file_chooser.set_files(audio_file)
-        # 配信の詳細
+        # 配信の詳細を設定
         await page.get_by_role("textbox", name="Title (required)", exact=True).fill(title)
         await page.get_by_role("textbox", name="", exact=True).fill(description)
         # スケジュール
@@ -59,17 +62,38 @@ async def publish(url: str, email: str, password: str, audio_file: str, title: s
             await promotional_group.get_by_role("radio", name="Yes", exact=True).set_checked(True, force=True)
         else:
             await promotional_group.get_by_role("radio", name="No", exact=True).set_checked(True, force=True)
-        # cookie ボタンの削除
+        additional_detail = page.get_by_role("button", name="Additional details (optional)")
+        # サムネイル差し替え時のボタンがcookieバナーに被って押せないので
+        # 先にcookie ボタンの削除
         await page.get_by_label("Cookie banner").get_by_label("Close").click()
+        # サムネイルを差し替え
+        if thumbnail:
+            if os.path.exists(thumbnail):
+                await additional_detail.click()
+                async with page.expect_file_chooser() as fc_info:
+                    await page.get_by_role("button", name="Episode cover art Change").click()
+                file_chooser = await fc_info.value
+                await file_chooser.set_files(thumbnail)
+                thumbnail_dialog = page.get_by_role("dialog", name="image uploader")
+                # 確定
+                await thumbnail_dialog.get_by_role("button", name="Save").click()
+
+            else:
+                print("Skip thumbnail replacement because the file does not exist.:{thumbnail}")
+
         await page.get_by_role("button", name="Next").click()
         # 投票機能など
         await page.get_by_role("button", name="Next").click()
-        # 公開
-        await page.get_by_role("button", name="Publish").click()
+        if is_publish:
+            # 公開
+            await page.get_by_role("button", name="Publish").click()
 
-        await page.wait_for_url(_publish_url)
-        await page.get_by_role("button", name="Copy link to your episode").click()
-        share_url = await page.evaluate("navigator.clipboard.readText()")
+            await page.wait_for_url(_publish_url)
+            await page.get_by_role("button", name="Copy link to your episode").click()
+            share_url = await page.evaluate("navigator.clipboard.readText()")
+        else:
+            print("Skip publish")
+            share_url = "Not Published."
         await page.close()
         await context.close()
         await browser.close()
